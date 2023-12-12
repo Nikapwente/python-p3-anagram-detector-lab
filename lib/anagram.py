1f8dcefcf7e6508bc8acbfae658aee8d54a24d3a
Nikapwente <hrsmajor@gmail.com>
2023-12-12 23:37:07 +0300

    Assignment Complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+class Anagram:
+    pass
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        result=[]
+        pass
+        for item in words:
+            if sorted([letter for letter in self.word]) == sorted([letter for letter in item]):
+                result.append(item)
+                print(result)
+            
+        return result
+
+test = Anagram("tab")
+test.match(["meat","atb","bat"])
